ab322d7ac19db2e8fa6b6d1f6f5aed492cae357c
Luke Wagoner <48413819+lwagoner@users.noreply.github.com>
2022-05-20 09:40:56 -0500

    Updating Tank movement

--- tutorial.py
+++ tutorial.py
@@ -1,24 +1,53 @@
 import os
 import pygame
+from pygame import Rect
+from pygame.math import Vector2
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 
-class Game():
+class GameState():
+    def __init__(self):
+        self.worldSize = Vector2(16,10)
+        self.tankPos = Vector2(0,0)
+
+
+    def update(self,moveTankCommand):
+        self.tankPos += moveTankCommand
+
+        if self.tankPos.x < 0:
+            self.tankPos.x = 0
+        elif self.tankPos.x >= self.worldSize.x:
+            self.tankPos.x = self.worldSize.x - 1
+
+        if self.tankPos.y < 0:
+            self.tankPos.y = 0
+        elif self.tankPos.y >= self.worldSize.y:
+            self.tankPos.y = self.worldSize.y - 1
+
+class UserInterface():
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode((640,480))
+        
+        self.gameState = GameState()
+        
+        self.cellSize = Vector2(64,64)
+        self.unitsTexture = pygame.image.load("units.png")
+        
+        windowSize = self.gameState.worldSize.elementwise() * self.cellSize
+        self.window = pygame.display.set_mode((int(windowSize.x),int(windowSize.y)))
+        
+        
         pygame.display.set_caption("Rylan's Rangers")
         pygame.display.set_icon(pygame.image.load("icon.png"))
+        self.moveTankCommand = Vector2(0,0)
+        
         self.clock = pygame.time.Clock()
-        self.x = 120
-        self.y = 120
         self.running = True
-        self.moveCommandX = 0
-        self.moveCommandY = 0
 
     def processInput(self):
-        self.moveCommandX = 0
-        self.moveCommandY = 0
+        
+        self.moveTankCommand = Vector2(0,0)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
@@ -28,22 +57,27 @@ class Game():
                     self.running = False
                     break
                 elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                    self.moveCommandX = 8
+                    self.moveTankCommand.x = 1
                 elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                    self.moveCommandX = 8
+                    self.moveTankCommand.x = -1
                 elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                    self.moveCommandY = 8
+                    self.moveTankCommand.y = 1
                 elif event.key == pygame.K_UP or event.key == pygame.K_w:
-                    self.moveCommandY = 8
+                    self.moveTankCommand.y = -1
                     
     def update(self):
-        self.x += self.moveCommandX
-        self.y += self.moveCommandY
+        self.gameState.update(self.moveTankCommand)
 
     def render(self):
         self.window.fill((0,0,0))
-        pygame.draw.circle(self.window,(0,255,0), (self.x,self.y), 50)
-        pygame.display.update()    
+        
+        # Tank base
+        spritePoint = self.gameState.tankPos.elementwise()*self.cellSize
+        texturePoint = Vector2(1,0).elementwise()*self.cellSize
+        textureRect = Rect(int(texturePoint.x), int(texturePoint.y), int(self.cellSize.x),int(self.cellSize.y))
+        self.window.blit(self.unitsTexture,spritePoint,textureRect)
+
+        pygame.display.update()       
 
     def run(self):    
         while self.running:
@@ -51,6 +85,8 @@ class Game():
             self.update()
             self.render()        
             self.clock.tick(60)
+            
+UserInterface = UserInterface()
+UserInterface.run()
 
-game = Game()
-game.run()
+pygame.quit()
